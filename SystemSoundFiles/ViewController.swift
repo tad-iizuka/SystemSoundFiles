274c53dd5f6d9d5b8a4626587862f5c13cef58d4
tad-iizuka <tad.iizuka@gmail.com>
2016-09-23 14:07:43 +0900

    Initial Commit

---
+++ SystemSoundFiles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SystemSoundFiles
+//
+//  Created by Tadashi on 2016/09/23.
+//  Copyright © 2016 T@d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
